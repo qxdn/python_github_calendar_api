debda5a95321dd431e56a5130db863287477e026
Xu Qian <1464238196@qq.com>
2024-03-30 22:38:29 +0800

    update index

--- api/index.py
+++ api/index.py
@@ -7,6 +7,7 @@ import json
 def list_split(items, n):
     return [items[i:i + n] for i in range(0, len(items), n)]
 def getdata(name):
+
     # 2024-03-29 定义 headers 请求头
     # 请见 https://github.com/yuhengwei2001/python_github_calendar_api/commit/0f37cfc003f09e99a1892602d8bc2b38137899d2#diff-b014e93fcab9bae29f453d7a616da5eac2f02947f32d02a1a1bf200eeaab5a39L11
     headers = {
@@ -21,11 +22,14 @@ def getdata(name):
         'X-Requested-With': 'XMLHttpRequest'
     }
     # 发送请求时添加 headers 请求头
-    #gitpage = requests.get("https://github.com/" + name)
+    # gitpage = requests.get("https://github.com/" + name)
     gitpage = requests.get("https://github.com/" + name  + "?action=show&controller=profiles&tab=contributions&user_id="+ name, headers=headers)
     data = gitpage.text
+    
+    # 2023-11-22 更新正则 https://github.com/Zfour/python_github_calendar_api/issues/18
     datadatereg = re.compile(r'data-date="(.*?)" id="contribution-day-component')
     datacountreg = re.compile(r'<tool-tip .*?class="sr-only position-absolute">(.*?) contribution')
+    
     datadate = datadatereg.findall(data)
     datacount = datacountreg.findall(data)
     datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
@@ -34,7 +38,7 @@ def getdata(name):
     if not datadate or not datacount:
         # 处理空数据情况
         return {"total": 0, "contributions": []}
-
+        
     # 将datadate和datacount按照字典序排序
     sorted_data = sorted(zip(datadate, datacount))
     datadate, datacount = zip(*sorted_data)
@@ -52,8 +56,12 @@ def getdata(name):
     return returndata
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        # 2024-03-15 规范接口的传参方式 https://github.com/Zfour/python_github_calendar_api/issues/20#issuecomment-1999115747
         path = self.path
-        user = path.split('?')[1]
+        spl=path.split('?')[1:]
+        for kv in spl:
+            key,user=kv.split("=")
+            if key=="user": break
         data = getdata(user)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
@@ -61,9 +69,3 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
         return
-
-if __name__ == '__main__':
-    from http.server import HTTPServer
-    server = HTTPServer(('localhost', 8080), handler)
-    while True:
-        server.handle_request()
